9721382f3ed7f63d7988f7209b4e048ec4fd8658
LesaN99 <phazima23@gmail.com>
2024-06-17 09:58:40 +0200

    Updated files

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'Ecommerce'
+
 urlpatterns = [
-    path('product_list/', views.product_list, name='product_list'),
+    path('', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>', views.customer_detail, name='customer_detail'),
+
 ]
